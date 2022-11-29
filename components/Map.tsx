52b3f425e3fb34174034bcb2f86a2ecfcddc2877
Berke Altıparmak <berkealtiparmak@outlook.com>
2022-11-29 11:46:20 +0300

    get lat lon on press

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,15 +1,30 @@
 import { View, Text, StyleSheet } from 'react-native'
-import React, { useMemo, useRef, useCallback } from 'react'
-import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps'
-import BottomSheet from '@gorhom/bottom-sheet';
+import React, { useMemo, useRef, useCallback, useState, useEffect } from 'react'
+import MapView, { PROVIDER_GOOGLE, Marker, Callout, MapMarker } from 'react-native-maps'
+import BottomSheet from '@gorhom/bottom-sheet'
+
 
 const Map: React.FunctionComponent = () => {
 
   const bottomSheetRef = useRef<BottomSheet>(null)
-  const snapPoints = useMemo(() => ['10%', '50%'], []) // two states-nothing between
+  const snapPoints = useMemo((): string[] => ['10%', '50%'], [])
   const handleSheetChanges = useCallback((index: number) => { console.log('handleSheetChanges', index) }, [])
 
-  // google maps api araştır
+  const [tappedLatLng, setTappedLatLng] = useState<{ latitude: number | null, longitude: number | null }>({
+    latitude: null,
+    longitude: null
+  })
+
+  let markers = [
+    {
+      latitude: 41.05,
+      longitude: 29.90,
+      title: 'Foo Place',
+      subtitle: '1234 Foo Drive'
+    }
+  ];
+
+  // google maps api araştır / tasarım çiz
   return (
     <View style={styles.container}>
       <MapView
@@ -17,24 +32,38 @@ const Map: React.FunctionComponent = () => {
         provider={PROVIDER_GOOGLE}
         showsUserLocation={true}
         followsUserLocation={true}
+        initialRegion={{
+          latitude: 41,
+          longitude: 29.015,
+          latitudeDelta: 0.2,
+          longitudeDelta: 0.2
+        }}
+        onPress={(e) => {
+          setTappedLatLng({
+            latitude: e.nativeEvent.coordinate.latitude,
+            longitude: e.nativeEvent.coordinate.latitude
+          })
+        }}
       >
         <Marker
           coordinate={{
             latitude: 41,
             longitude: 29
           }}
+          pinColor={'crimson'}
           title={"Test"}
           description={"Test description"}
         />
+
       </MapView>
-      <BottomSheet
+      {/* <BottomSheet
         ref={bottomSheetRef}
         index={1}
         snapPoints={snapPoints}
         onChange={handleSheetChanges}
       >
 
-      </BottomSheet>
+      </BottomSheet> */}
     </View>
   )
 }
